e1e9b2a863d635d30997f657dc123c032c09ad89
irwanazam <irwan.azam@enovade.com>
2024-07-24 13:43:32 +0800

    Refactor States

---
+++ BlazorTrainingDemo/Modules/Maintenances/States/GetStates.cs
@@ -0,0 +1,26 @@
+﻿using BlazorTrainingDemo.Data;
+using BlazorTrainingDemo.Modules.Products;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorTrainingDemo.Modules.States
+{
+    public record GetStatesQuery() : IRequest<List<State>>;
+
+    public class GetStatesHandler : IRequestHandler<GetStatesQuery, List<State>>
+    {
+        private readonly ApplicationDbContext _context;
+
+
+        public GetStatesHandler(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<State>> Handle(GetStatesQuery request, CancellationToken cancellationToken)
+        {
+            //Validation or other business processes
+
+            return await _context.States.AsNoTracking().ToListAsync(cancellationToken);
+        }
+    }
+}
