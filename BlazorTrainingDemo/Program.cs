fb51439738e92845bf650f0eac1b660d180dd96b
irwanazam <irwan.azam@enovade.com>
2024-07-24 12:55:16 +0800

    Add fluentui datagrid services

--- BlazorTrainingDemo/Program.cs
+++ BlazorTrainingDemo/Program.cs
@@ -14,6 +14,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 builder.Services.AddFluentUIComponents();
+builder.Services.AddDataGridEntityFrameworkAdapter();
 
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddScoped<IdentityUserAccessor>();
