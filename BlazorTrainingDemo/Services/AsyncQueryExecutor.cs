b616eff4d236b0734b4aa2a5fae4ba1fb5e888a9
Muhammad Aqhil Hussin <muhdaqhil21@gmail.com>
2024-07-24 12:54:06 +0800

    add service

---
+++ BlazorTrainingDemo/Services/AsyncQueryExecutor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorTrainingDemo.Services
+{
+    public class AsyncQueryExecutor<TEntity> : IAsyncQueryExecutor<TEntity> where TEntity : class
+    {
+        public Task<int> CountAsync(IQueryable<TEntity> source, CancellationToken cancellationToken = default)
+        {
+            return source.CountAsync(cancellationToken);
+        }
+
+        public Task<List<TEntity>> ToListAsync(IQueryable<TEntity> source, CancellationToken cancellationToken = default)
+        {
+            return source.ToListAsync(cancellationToken);
+        }
+    }
+
+    public interface IAsyncQueryExecutor<TEntity> where TEntity : class
+    {
+        Task<int> CountAsync(IQueryable<TEntity> source, CancellationToken cancellationToken = default);
+        Task<List<TEntity>> ToListAsync(IQueryable<TEntity> source, CancellationToken cancellationToken = default);
+    }
+}
